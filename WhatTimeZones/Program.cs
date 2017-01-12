214c692ef1fabc5c55092e049ed64ba42f20b234
Mark Smith <msmith@intelligentenvironments.com>
2017-01-12 10:25:16 +0000

    Initial Commit

---
+++ WhatTimeZones/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace WhatTimeZones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeZoneInfo.GetSystemTimeZones().ToList()
+                .ForEach(x => 
+                Console.WriteLine($"-----------------------------------------{Environment.NewLine}" +
+                                  $"ID: {x.Id} {Environment.NewLine}" +
+                                  $"DisplayName: {x.DisplayName}{Environment.NewLine}" +
+                                  $"StandardName: {x.StandardName}"));
+
+            Console.ReadLine();
+
+        }
+    }
+}
